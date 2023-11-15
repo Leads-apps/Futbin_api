4245dcf9dcf907cee41487d64be936d0c439d783
Oleksandr Solokha <s.s@dcjudgement.com>
2023-11-15 19:07:31 +0200

    🐣(Player) feat data point

--- Sources/Futbin_api/Player.swift
+++ Sources/Futbin_api/Player.swift
@@ -2,28 +2,28 @@ import Foundation
 
 public struct Player {
     
-    let name : String
-    let playerImage : String
-    let clubName : String
-    let clubImage : String
-    let nationality : String
-    let natioalityFlag : String
-    let leagueName : String
-    let leagueImage : String
-    let rate : Int
-    let position : String
-    let playerPrice : String
-    let skills : Int
-    let weakFoot : Int
-    let attackDefense : String
-    let pace : Int
-    let shooting : Int
-    let passing : Int
-    let dribbling : Int
-    let defending : Int
-    let physicality : Int
-    let heightWeight : String
-    let popularity : Int
-    let baseStats : Int
-    let gameStats : Int
+    public let name : String
+    public let playerImage : String
+    public let clubName : String
+    public let clubImage : String
+    public let nationality : String
+    public let natioalityFlag : String
+    public let leagueName : String
+    public let leagueImage : String
+    public let rate : Int
+    public let position : String
+    public let playerPrice : String
+    public let skills : Int
+    public let weakFoot : Int
+    public let attackDefense : String
+    public let pace : Int
+    public let shooting : Int
+    public let passing : Int
+    public let dribbling : Int
+    public let defending : Int
+    public let physicality : Int
+    public let heightWeight : String
+    public let popularity : Int
+    public let baseStats : Int
+    public let gameStats : Int
 }
