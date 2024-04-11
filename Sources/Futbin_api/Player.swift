7130eb3ef035f44d97e82eb1020f7f475c08f59c
Oleksandr Solokha <s.s@dcjudgement.com>
2024-04-11 11:43:46 +0300

    🐣(Player) feat public init

--- Sources/Futbin_api/Player.swift
+++ Sources/Futbin_api/Player.swift
@@ -8,6 +8,20 @@ public struct Player {
     public let popularity : String?
     public let baseStats : String?
     public let gameStats : String?
+    
+    public init(linkDetail: String,
+                name: String? = nil,
+                playerPrice: String? = nil,
+                popularity: String? = nil,
+                baseStats: String? = nil,
+                gameStats: String? = nil) {
+        self.linkDetail = linkDetail
+        self.name = name
+        self.playerPrice = playerPrice
+        self.popularity = popularity
+        self.baseStats = baseStats
+        self.gameStats = gameStats
+    }
 }
 
 public struct PlayerDetail: Identifiable {
@@ -39,31 +53,57 @@ public struct PlayerDetail: Identifiable {
     public var height : String?
     public var weight : String?
     
-    init() {
-        id = UUID().uuidString
-        name = nil
-        playerImage = nil
-        cardImage = nil
-        clubName = nil
-        clubImage = nil
-        nationality = nil
-        natioalityFlag = nil
-        leagueName = nil
-        leagueImage = nil
-        rareType = nil
-        rate = nil
-        position = nil
-        skills = nil
-        weakFoot = nil
-        attackWR = nil
-        defenseWR = nil
-        pace = nil
-        shooting = nil
-        passing = nil
-        dribbling = nil
-        defending = nil
-        physicality = nil
-        height = nil
-        weight = nil
+    public init(player: Player? = nil,
+                id: String = UUID().uuidString,
+                name: String? = nil,
+                playerImage: String? = nil,
+                cardImage: String? = nil,
+                clubName: String? = nil,
+                clubImage: String? = nil,
+                nationality: String? = nil,
+                natioalityFlag: String? = nil,
+                leagueName: String? = nil,
+                leagueImage: String? = nil,
+                rareType: String? = nil,
+                rate: String? = nil,
+                position: String? = nil,
+                skills: String? = nil,
+                weakFoot: String? = nil,
+                attackWR: String? = nil,
+                defenseWR: String? = nil,
+                pace: String? = nil,
+                shooting: String? = nil,
+                passing: String? = nil,
+                dribbling: String? = nil,
+                defending: String? = nil,
+                physicality: String? = nil,
+                height: String? = nil,
+                weight: String? = nil) {
+        self.player = player
+        self.id = id
+        self.name = name
+        self.playerImage = playerImage
+        self.cardImage = cardImage
+        self.clubName = clubName
+        self.clubImage = clubImage
+        self.nationality = nationality
+        self.natioalityFlag = natioalityFlag
+        self.leagueName = leagueName
+        self.leagueImage = leagueImage
+        self.rareType = rareType
+        self.rate = rate
+        self.position = position
+        self.skills = skills
+        self.weakFoot = weakFoot
+        self.attackWR = attackWR
+        self.defenseWR = defenseWR
+        self.pace = pace
+        self.shooting = shooting
+        self.passing = passing
+        self.dribbling = dribbling
+        self.defending = defending
+        self.physicality = physicality
+        self.height = height
+        self.weight = weight
     }
 }
